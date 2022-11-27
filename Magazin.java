3435b05bad2a7ed5b43ece05c03c2f97ef7e5a10
MAKS <apomaksim@gmail>
2022-11-28 03:27:45 +0700

    bbb

--- Magazin.java
+++ Magazin.java
@@ -1,8 +1,15 @@
-//package LAB3;
+//package LAB3; 
 
-
+import java.util.*;
+import java.util.Collections;
+import java.util.List;
 import java.lang.String;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
+
+import javax.swing.table.TableRowSorter;
 
 public class Magazin {
     public static String user;
@@ -15,12 +22,48 @@ public class Magazin {
         Setuser(); SetTovar(); setadres(); setdata();
         Getuser(); GetTovar(); Getadres(); Getdata();
 
+        ArrayList<Ages> Konst = new ArrayList<Ages>();
+
+        Konst.add(new Ages("Rodot", 12000));
+        Konst.add(new Ages("Rodot", 33333));
+        Konst.add(new Ages("Boss", 234));
+        Konst.add(new Ages("Director", 1000000));
+
+        Collections.sort(Konst);
+          
+        System.out.println("\n Сортировка: \n\n");
+
+        for (Object o : Konst){
+            System.out.println(o);
+        }
+
+        System.out.println("\n Поиск: \n\n");
+
+        Comparator<Ages> c = new Comparator<Ages>() {
+            public int compare(Ages u1, Ages u2)
+            {
+                return u1.getZp().compareTo(u2.getZp());
+            }
+        };
+
+
+        int index = Collections.binarySearch(Konst, new Ages(null, 234), c);
+
+        if (index >= 0){
+            System.out.println("Работник с такой зп найден!!!");
+        }
+        else{
+            System.out.println("Работник с такой зп не найден!!!");
+        }
+        
+       // System.out.println(index);
+
     }
 
     public static void Setuser(){
 
         
-        User set = new User();
+        User set = new User("Boss");
         set.setUser();
 
     }
@@ -76,5 +119,14 @@ public class Magazin {
         System.out.print(dates);
         
     }
+    
+    public static void Ages()
+    {
+        Ages set = new Ages("Boss", 999999);
+        set.getUser();
+        set.getUser(34);
+    }
 
 }
+
+
--- User.java
+++ User.java
@@ -2,24 +2,37 @@ import java.lang.String;
 import java.util.Scanner;
 
 public class User {
-    
-        public void setUser(){
-        
-      
 
-        System.out.print("Введите свое имя: ");
-        Scanner name = new Scanner(System.in);
-        Magazin.user = name.nextLine();
-        
-        System.out.print("Введите свою фамилию: ");
-        Scanner surname = new Scanner(System.in);
-        Magazin.user = Magazin.user + " " + surname.nextLine();
-        
-        
+        String position;
+
+                public User(String position){
+                this.position = position;
         }
 
+        public String names;
 
-    
+
+
+        public void setUser(){
+
+
+
+                System.out.print("Введите свое имя: ");
+                Scanner name = new Scanner(System.in);
+                Magazin.user = name.nextLine();
+
+                System.out.print("Введите свою фамилию: ");
+                Scanner surname = new Scanner(System.in);
+                Magazin.user = Magazin.user + " " + surname.nextLine();
+
+
+        }
+
+        public void NAmme(){
+                names = Magazin.user;
+                System.out.print(names);
+        }
+
 
 
 
